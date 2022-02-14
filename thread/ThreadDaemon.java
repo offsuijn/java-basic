b1cbc10023352cdf9fc1bca71abe1590f32b066e
offsuijn <88263178+offsuijn@users.noreply.github.com>
2022-02-14 21:36:46 +0900

    Create ThreadDaemon.java

---
+++ thread/ThreadDaemon.java
@@ -0,0 +1,39 @@
+public class ThreadDaemon implements Runnable {
+    static boolean autoSave = false;
+
+    public static void main(String[] args) {
+        Thread t = new Thread(new ThreadDaemon());
+        t.setDaemon(true);
+        t.start();
+
+        for(int i = 0; i <= 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            System.out.println(i);
+
+            if (i == 5) {
+                autoSave = true;
+            }
+        }
+
+        System.out.println("프로그램을 종료합니다.");
+    }
+
+    public void run() {
+        while(true) {
+            try {
+                Thread.sleep(3000);
+            } catch(InterruptedException e) {}
+
+            if (autoSave == true) {
+                autoSave();
+            }
+        }
+    }
+
+    public void autoSave() {
+        System.out.println("작업파일이 자동저장되었습니다.");
+    }
+}
